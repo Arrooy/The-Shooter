b48e9783c7a445bc9b7c60c36e0ca6bf5c76998a
Arrooy <adria.arroyo@outlook.es>
2021-04-16 16:54:41 +0200

    Acabat info del FAT.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::fs;
-use std::env;
-use std::str;
 use std::borrow::Borrow;
+use std::env;
 use std::fmt::Display;
+use std::fs;
 use std::fs::File;
+use std::str;
 use std::str::Utf8Error;
 
 const RESOURCES_PATH: &str = "./res/";
@@ -18,21 +18,58 @@ const ERROR_OPTION_NOT_FOUND: &str = "Opcio no reconeguda! Opcions reconegudes s
 
 const ERROR_VOLUME_FORMAT_NOT_RECOGNIZED: &str = "Error. No es possible reconeixer el format del volum";
 
+const INFO_HEADER: &str = "------ Filesystem Information ------";
 
-enum SupportedFilesystems {
-    FAT16,
-    EX2,
+struct GenericVolume {
+    data: Vec<u8>,
+    file_name: Option<String>,
 }
 
-trait Filesystem{
-    fn new(volume_name: String, file_name: Option<String>) -> Self;
+impl GenericVolume {
+    fn new(volume_name: String, file_name: Option<String>) -> Self {
+        Self {
+            data: fs::read(format!("{}{}", RESOURCES_PATH, volume_name)).expect(format!("{}{}", ERROR_VOLUME_NOT_FOUND, volume_name).as_str()),
+            file_name,
+        }
+    }
+    // print!("{:#04x}",&self.data[82..=90]);
+    // print!("{:?}",&self.data[3..=8]);
+    // Box<dyn Filesystem>
+
+    fn is_fat(&self) -> bool {
+        // BS_FilSysType contains FAT (One of the strings “FAT12 ”, “FAT16 ”, or “FAT ”.) in position 54.
+        // Check sector 510 == 0x55 and sector 511 == 0xAA
+        extract_string(&self.data, 54, 8).unwrap().contains("FAT") && self.data[510..=511] == [0x55, 0xAA]
+    }
+
+    fn is_ex2(&self) -> bool {
+        todo!()
+    }
+}
+
+fn extract_string(data: &[u8], base: usize, offset: usize) -> Result<&str, Utf8Error> {
+    str::from_utf8(&data[base..base + offset])
+}
+
+fn extract_u16(data: &[u8], base: usize, offset: usize) -> u16 {
+    let vec = &data[base..base + offset];
+    ((vec[0] as u16) << 8) | vec[1] as u16
+}
+
+fn extract_u32(data: &[u8], base: usize, offset: usize) -> u32 {
+    let vec = &data[base..base + offset];
+    ((vec[0] as u32) << 24) | ((vec[1] as u32) << 16) | ((vec[2] as u32) << 8) | (vec[3] as u32)
+}
+
+trait Filesystem {
+    fn new(gv: GenericVolume) -> Self;
 
     fn process_operation(&self, operation: String) {
         match operation.as_str() {
             "/info" => self.info(),
             "/find" => todo!(),
             "/delete" => todo!(),
-            _ => print!("{}",ERROR_OPTION_NOT_FOUND),
+            _ => print!("{}", ERROR_OPTION_NOT_FOUND),
         }
     }
 
@@ -41,58 +78,63 @@ trait Filesystem{
     fn delete(&self, file_name: &'static str);
 }
 
-struct GenericVolume {
-    data: Vec<u8>,
-    file_name: Option<String>
-}
-
-impl GenericVolume{
-    fn new(volume_name: String, file_name: Option<String>) -> Self {
-        Self {
-            data: fs::read(format!("{}{}", RESOURCES_PATH, volume_name)).expect(format!("{}{}",ERROR_VOLUME_NOT_FOUND, volume_name).as_str()),
-            file_name
-        }
-    }
-    // print!("{:#04x}",&self.data[82..=90]);
-    // print!("{:?}",&self.data[3..=8]);
-    // Box<dyn Filesystem>
-
-    fn is_fat(&self) -> bool{
-        // BS_FilSysType contains FAT (One of the strings “FAT12 ”, “FAT16 ”, or “FAT ”.) in position 54.
-        // Check sector 510 == 0x55 and sector 511 == 0xAA
-        extract(&self.data, 54, 8).unwrap().contains("FAT") && self.data[510..=511] == [0x55,0xAA]
-    }
-
-    fn is_ex2(&self) -> bool{
-        todo!()
-    }
-}
-
-fn extract(data: &[u8], base: usize, offset: usize) -> Result<&str, Utf8Error>  {
-    str::from_utf8(&data[base..=base+offset])
-}
-
 struct FAT16 {
-    data: Vec<u8>,
     file_name: Option<String>,
     bpb_byts_per_sec: u16,
     bpb_sec_per_clus: u8,
     bpb_rsvd_sec_cnt: u16,
     bpb_num_fats: u8,
     bpb_root_ent_cnt: u16,
-    bpb_tot_sec16: u16, // Un dels dos ha de ser non zero!
-    bpb_tot_sec32: u32,  // Un dels dos ha de ser non zero!
+    bpb_tot_sec16: u16,
+    // Un dels dos ha de ser non zero!
+    bpb_tot_sec32: u32,
+    // Un dels dos ha de ser non zero!
     bs_vol_lab: String,
+    bs_oemname: String,
+    data: Vec<u8>,
 }
 
 impl Filesystem for FAT16 {
-    //TODO: Crear constructor desde genericVol.
-    fn new(volume_name: String, file_data: Option<String>) -> Self {
-       todo!()
+    fn new(gv: GenericVolume) -> Self {
+        FAT16 {
+            file_name: gv.file_name,
+
+            bpb_byts_per_sec: extract_u16(&gv.data, 11, 2),
+            bpb_sec_per_clus: gv.data[13],
+            bpb_rsvd_sec_cnt: extract_u16(&gv.data, 14, 2),
+            bpb_num_fats: gv.data[16],
+            bpb_root_ent_cnt: extract_u16(&gv.data, 17, 2),
+            bpb_tot_sec16: extract_u16(&gv.data, 19, 2),
+            bpb_tot_sec32: extract_u32(&gv.data, 32, 4),
+            bs_vol_lab: extract_string(&gv.data, 54, 8).unwrap().parse().unwrap(),
+            bs_oemname: extract_string(&gv.data, 3, 8).unwrap().parse().unwrap(),
+            data: gv.data,
+        }
     }
 
-    fn info(&self){
-
+    fn info(&self) {
+        print!("{}
+Filesystem: FAT16
+System Name: {}
+Mida del sector: {}
+Sectors Per Cluster: {}
+Sectors reservats: {}
+Número de FATs (16): {}
+Número de FATs (32): {}
+MaxRootEntries: {}
+Sectors per FAT: {}
+Label: {}",
+            //TODO: REORDENAR!
+               INFO_HEADER,
+               self.bs_oemname,
+               self.bpb_byts_per_sec,
+               self.bpb_sec_per_clus,
+               self.bpb_rsvd_sec_cnt,
+               self.bpb_num_fats,
+               self.bpb_root_ent_cnt,
+               self.bpb_tot_sec16,
+               self.bpb_tot_sec32,
+               self.bs_vol_lab)
     }
 
     fn find(&self, file_name: &'static str) {
@@ -109,16 +151,16 @@ fn main() {
     let volume_name = env::args().nth(2).expect("");
     let file_name = env::args().nth(3);
 
-     // Create a new FileSystem
+    // Create a new FileSystem
     let unkown_vol = GenericVolume::new(volume_name, file_name);
-    let filesystem: dyn Filesystem;
+    let filesystem;
 
     if unkown_vol.is_fat() {
-        filesystem = FAT16::new(unkown_vol.)
-    }else if unkown_vol.is_ex2() {
-        todo!()
-    }else{
-        print!(ERROR_VOLUME_FORMAT_NOT_RECOGNIZED)
+        filesystem = FAT16::new(unkown_vol)
+    } else if unkown_vol.is_ex2() {
+        filesystem = FAT16::new(unkown_vol)
+    } else {
+        panic!("{}", ERROR_VOLUME_FORMAT_NOT_RECOGNIZED)
     }
 
     filesystem.process_operation(operation)
