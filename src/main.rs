b0993f007eca570b67cb18317e1ff86bab2bdfb8
Arrooy <adria.arroyo@outlook.es>
2021-04-28 13:25:40 +0200

    Acabat FAT16 find fase 2

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,19 @@ use std::str;
 use std::str::Utf8Error;
 
 use chrono::prelude::*;
-
+use core::fmt;
 /*
     Fat analysis
-    fatcat -i Fat16_1024
+    fatcat -i Fat16
 
     Ex2 analysis
     dumpe2fs -h Ext2
 
 */
 
-// TODO: FAT16 no concorda el camp SectorsxFAT
-// TODO: preguntar si es pot fer la conversio de temps amb libs.
-
 const RESOURCES_PATH: &str = "./res/";
 
-const ERROR_VOLUME_NOT_FOUND: &str = "Error! No s'ha trobat el volum";
+const ERROR_VOLUME_NOT_FOUND: &str = "Error. Volum inexistent";
 
 const ERROR_NUM_ARGS: &str = "Error amb el nombre d'arguments. El programa espera 2 o 3 arguments.\
 Per l'informació d'un volum prova: cargo run /info vol_name\
@@ -29,7 +26,7 @@ Per buscar un fitxer prova: cargo run /find vol_name file_name.txt";
 
 const ERROR_OPTION_NOT_FOUND: &str = "Opcio no reconeguda! Opcions reconegudes són /info /find /delete";
 
-const ERROR_VOLUME_FORMAT_NOT_RECOGNIZED: &str = "Sistema d’arxius no és ni EXT2 ni FAT16";
+const ERROR_VOLUME_FORMAT_NOT_RECOGNIZED: &str = "Error. Volum no formatat en FAT16 ni EX2.";
 
 const INFO_HEADER: &str = "------ Filesystem Information ------";
 
@@ -41,7 +38,7 @@ struct GenericVolume {
 impl GenericVolume {
     fn new(volume_name: String, file_name: Option<String>) -> Self {
         Self {
-            data: fs::read(format!("{}{}", RESOURCES_PATH, volume_name)).expect(format!("{} {}", ERROR_VOLUME_NOT_FOUND, volume_name).as_str()),
+            data: fs::read(format!("{}{}", RESOURCES_PATH, volume_name)).expect(format!("{}", ERROR_VOLUME_NOT_FOUND).as_str()),
             file_name,
         }
     }
@@ -69,6 +66,7 @@ impl GenericVolume {
 fn extract_string(data: &[u8], base: usize, offset: usize) -> Result<&str, Utf8Error> {
     str::from_utf8(&data[base..base + offset])
 }
+
 // Extrau un string fins a trobar un \0
 fn extract_string_terminated(data: &[u8], base: usize, offset: usize) -> Result<&str, Utf8Error> {
     let vec =  &data[base..base + offset];
@@ -81,16 +79,15 @@ fn extract_string_terminated(data: &[u8], base: usize, offset: usize) -> Result<
     }
 }
 
-
 fn extract_u16(data: &[u8], base: usize) -> u16 {
     let vec = &data[base..base + 2];
-    // println!("Extracting [{}..{}] is {:?}", base, 2, vec);
+    //println!("Extracting [{}..{}] is {:?}", base, 2, vec);
     ((vec[1] as u16) << 8) | vec[0] as u16
 }
 
 fn extract_u32(data: &[u8], base: usize) -> u32 {
     let vec = &data[base..base + 4];
-    // println!("Extracting [{}..{}] is {:?}", base, 4, vec);
+    //println!("Extracting [{}..{}] is {:?}", base, 4, vec);
     ((vec[3] as u32) << 24) | ((vec[2] as u32) << 16) | ((vec[1] as u32) << 8) | (vec[0] as u32)
 }
 
@@ -103,6 +100,7 @@ fn timestamp_to_date_time(timestamp: u32) -> String {
     let time: DateTime<Utc> = DateTime::from_utc(naive_datetime, Utc);
     time.format("%a %b %e %T %Y").to_string()
 }
+
 
 trait Filesystem {
     fn new(gv: GenericVolume) -> Self
@@ -124,45 +122,104 @@ trait Filesystem {
 
 struct FAT16 {
     file_name: Option<String>,
-    bytes_per_sector: u16,
-    num_sec_per_alloc: u8,
+    bpb_byts_per_sec: u16,
+    bpb_sec_per_clus: u8,
     num_rsvd_sec: u16,
-    num_fats: u8,
-    num_root_dir: u16,
-    num_total_sec: u32,
+    bpb_num_fats: u8,
+    bpb_root_ent_cnt: u16,
+    bpb_fatsz16: u16,
     bs_vol_lab: String,
     oem_name: String,
     data: Vec<u8>,
+    root_dir_sectors: u16,
+    first_data_sector:u32,
+    data_sec: u32,
+}
+
+enum FatType {
+    FAT12,
+    FAT16,
+    FAT32,
+}
+
+impl fmt::Display for FatType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FatType::FAT12 => write!(f, "FAT12"),
+            FatType::FAT16 => write!(f, "FAT16"),
+            FatType::FAT32 => write!(f, "FAT32"),
+        }
+    }
+}
+
+impl FAT16{
+    fn get_fat_type(&self) -> FatType {
+
+        return match self.data_sec / self.bpb_sec_per_clus as u32 {
+            count_of_clusters if count_of_clusters < 4085 => FatType::FAT12,
+            count_of_clusters if count_of_clusters <  65525 => FatType::FAT16,
+            _ =>  FatType::FAT32
+        }
+    }
+
+    fn get_sec(&self, n: u32) -> u32{
+        let fat_offset = n * 2;
+
+        let fat_sec_num = self.num_rsvd_sec as u32 + (fat_offset / self.bpb_byts_per_sec as u32);
+        let fat_ent_offset = fat_offset % self.bpb_byts_per_sec as u32;
+        print!("Fat offset is {}", fat_ent_offset);
+        12
+    }
 }
 
 impl Filesystem for FAT16 {
     fn new(gv: GenericVolume) -> Self {
-        let num_total_sec: u32;
 
-        // Mirem si el nombre de sectors de 32 bits esta buit. Sino el fem servir.
-        if extract_u32(&gv.data, 32) == 0 {
-            num_total_sec = extract_u16(&gv.data, 19) as u32;
-        } else {
-            num_total_sec = extract_u32(&gv.data, 32);
-        }
+        let bpb_root_ent_cnt = extract_u16(&gv.data, 17);
+        let bpb_byts_per_sec = extract_u16(&gv.data, 11);
+        let num_rsvd_sec = extract_u16(&gv.data, 14);
+        let bpb_num_fats = gv.data[16];
+        let bpb_fatsz16 = extract_u16(&gv.data, 22);
+
+        // Calcul del nombre de sectors que ocupa el root directory.
+        let root_dir_sectors = ((bpb_root_ent_cnt * 32) + (bpb_byts_per_sec - 1)) / bpb_byts_per_sec;
+        // Start of data sector
+        let first_data_sector = num_rsvd_sec as u32 + (bpb_num_fats as u32 * bpb_fatsz16 as u32) + root_dir_sectors as u32;
+
+
+        let bpb_tot_sec16 =  extract_u16(&gv.data, 19);
+
+        let bpb_tot_sec = if bpb_tot_sec16 == 0 {
+            extract_u32(&gv.data, 32)
+        }else{
+            bpb_tot_sec16 as u32
+        };
+
+        // Count of sectors in data region
+        let data_sec: u32 =  bpb_tot_sec - first_data_sector;
 
         FAT16 {
             file_name: gv.file_name,
-            bytes_per_sector: extract_u16(&gv.data, 11),
-            num_sec_per_alloc: gv.data[13],
-            num_rsvd_sec: extract_u16(&gv.data, 14),
-            num_fats: gv.data[16],
-            num_root_dir: extract_u16(&gv.data, 17),
-            num_total_sec,
+            bpb_byts_per_sec,
+            bpb_sec_per_clus: gv.data[13],
+            num_rsvd_sec,
+            bpb_num_fats,
+            bpb_root_ent_cnt,
+            bpb_fatsz16,
             bs_vol_lab: extract_string(&gv.data, 43, 11).unwrap().parse().unwrap(),
             oem_name: extract_string(&gv.data, 3, 8).unwrap().parse().unwrap(),
             data: gv.data,
+            root_dir_sectors,
+            first_data_sector,
+            data_sec,
         }
     }
 
     fn info(&self) {
-        println!("{}\n
-Filesystem: FAT16
+
+
+            println!("{}\n
+Filesystem: {}
 System Name: {}
 Mida del sector: {}
 Sectors Per Cluster: {}
@@ -171,20 +228,83 @@ Número de FATs: {}
 MaxRootEntries: {}
 Sectors per FAT: {}
 Label: {}",
-               INFO_HEADER,
-               self.oem_name,
-               self.bytes_per_sector,
-               self.num_sec_per_alloc,
-               self.num_rsvd_sec,
-               self.num_fats,
-               self.num_root_dir,
-               self.num_total_sec,
-               self.bs_vol_lab)
+                     INFO_HEADER,
+                    self.get_fat_type(),
+                     self.oem_name,
+                     self.bpb_byts_per_sec,
+                     self.bpb_sec_per_clus,
+                     self.num_rsvd_sec,
+                     self.bpb_num_fats,
+                     self.bpb_root_ent_cnt,
+                     self.bpb_fatsz16,
+                     self.bs_vol_lab)
+
+
     }
 
     fn find(&self) {
-        todo!()
+
+        if let None = self.file_name {
+            panic!("File name not specified!")
+        }
+
+        let query_filename = self.file_name.as_ref().unwrap();
+
+        match self.get_fat_type() {
+            FatType::FAT12 => panic!("Filesystem must be FAT16. FAT12 found instead!"),
+            FatType::FAT16 => (),
+            FatType::FAT32 => println!("Filesystem must be FAT16. FAT32 found instead!"),
+        }
+
+        let first_root_dir_sec_num = self.num_rsvd_sec as u32 + ( self.bpb_num_fats as u32 * self.bpb_fatsz16 as u32);
+        let first_root_dir_start = first_root_dir_sec_num * self.bpb_byts_per_sec as u32;
+        let first_root_dir_end = (self.root_dir_sectors as u32 * self.bpb_byts_per_sec as u32) + first_root_dir_start;
+
+        let mut i :u32 = first_root_dir_start;
+        let mut found:bool = false;
+
+        while i < first_root_dir_end {
+            let directory = &self.data[i as usize..(i+32) as usize];
+
+            // No hi ha info en aquest bloc. Anem al seguent
+            if directory[0] == 0xE5 {
+                i += 32;
+                continue;
+            }
+
+            // No hi ha info en el bloc ni en en el seguents
+            if directory[0] == 0x00 {
+                break;
+            }
+
+            // Hem trobat un directori!
+            let nom = extract_string(directory, 0, 8);
+            let extension = extract_string(directory, 8, 3);
+            let filename = (nom.unwrap().replace(" ","") + "." + extension.unwrap()).to_lowercase();
+            let attr = directory[11];
+
+            /* Check nom llarg.
+            if attr == 0xf{
+                print!("El nom es LONG NAME!")
+            }
+             */
+
+            let cluster_numbers = (directory[26] as u16) << 8 | (directory[27] as u16);
+            let file_size = extract_u32(directory,28);
+
+            if *query_filename == filename {
+                println!("Fitxer trobat! Ocupa {} bytes.", file_size);
+                found = true;
+                break;
+            }
+            //println!("Info found:\n {:?} -- {:?} {:?} {:#04x} {:?} {:?}", filename, nom , extension, attr, cluster_numbers, file_size);
+            i += 32;
+        }
+        if !found {
+            println!("Error. Fitxer no trobat.")
+        }
     }
+
     fn delete(&self) {
         todo!()
     }
